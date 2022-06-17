817eb788e32a828cb0936de7da74dc7768679245
unknown <lourivalformal@gmail.com>
2022-06-17 14:49:07 -0300

    cadastrar frutas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { CadastroForm } from "../pages/Cadastro"
 import { Login } from "../pages/Login"
 import { HomeInicial } from "../pages/Home"
+import { Painel } from "../pages/fruta.jsx"
 
 
 export const AppRouter = () => {
@@ -11,6 +12,7 @@ export const AppRouter = () => {
                 <Route path="/login" exact element={<Login />} />
                 <Route path="/cadastro" exact element={<CadastroForm />} />
                 <Route path="/home" exact element={<HomeInicial />} /> 
+                <Route path="/painel" exact element={<Painel />} /> 
             </Routes>
         </Router>
 
